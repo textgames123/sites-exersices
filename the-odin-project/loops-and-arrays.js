6f56875c0f708febdb4398ca226095eabca9ebb3
Text Games <297672581+textgames123@users.noreply.github.com>
2026-07-21 12:58:23 +0300

    Add a new way of getting the sum of even numbers

--- the-odin-project/loops-and-arrays.js
+++ the-odin-project/loops-and-arrays.js
@@ -9,6 +9,15 @@ function sumOfTripleNumbers(numbers) {
   return sum;
 }
 
+function sumOfTripleNumbersUsingArray(numbers) {
+  let evenNumbers = numbers.filter((currentNumber) => currentNumber % 2 === 0);
+  let tripledNumbers = evenNumbers.map((currentNumber) => currentNumber * 3);
+  let sumOfTripledNumbers = tripledNumbers.reduce((total, currentNumber) => total + currentNumber);
+  return sumOfTripledNumbers;
+}
+
+
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(sumOfTripleNumbers(arr));
+console.log(sumOfTripleNumbersUsingArray(arr));
